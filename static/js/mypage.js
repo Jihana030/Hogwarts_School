5a0d9beafe59eb1e7e54ca7d669a86672023740d
hyelin <myelin0213@gmail.com>
2023-01-30 23:57:36 +0900

    23/01/30 23:57 [2]

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -7,12 +7,17 @@
         const calendar = new FullCalendar.Calendar($calendar, {
             expandRows: true,
             initialView: 'dayGridMonth',
-        })
+            fixedWeekCount: false,
+        });
         calendar.render();
 
         const $calendarMonth = $calendar.querySelector('#fc-dom-1');
         $calendarMonth.style.textTransform = 'uppercase';
-        console.log($calendarMonth)
+
+        const $calendarDay = $calendar.querySelectorAll('.fc-col-header-cell-cushion');
+        $calendarDay.forEach(day => {
+            day.textContent = day.textContent.substring(0, 1);
+        })
     });
 
 
