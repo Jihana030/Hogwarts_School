88950d0b7c5cecb79afa690c9ebda4f52e7340be
hyelin <myelin0213@gmail.com>
2023-01-30 18:06:45 +0900

    23/01/30 18:06 [2]

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -1,18 +1,20 @@
-(function() {
+(function () {
 
     'use strict';
-    
+
     const $calendar = document.querySelector('#calendar');
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         const calendar = new FullCalendar.Calendar($calendar, {
             expandRows: true,
             initialView: 'dayGridMonth',
         })
         calendar.render();
-    });
-    
-    const $calendarMonth = $calendar.querySelector('#fc-dom-1');
 
-    console.log($calendarMonth)
+        const $calendarMonth = $calendar.querySelector('#fc-dom-1');
+        $calendarMonth.style.textTransform = 'uppercase';
+        console.log($calendarMonth)
+    });
+
+
 
 })();
