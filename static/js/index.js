79c04726ac31242951174ebabe125736328ac45a
JeongWon Yu <galleon32122@naver.com>
2023-02-04 18:49:31 +0900

    lumos

--- static/js/index.js
+++ static/js/index.js
@@ -79,6 +79,36 @@
     $searchFront.value = value;
   }
 
+  // ??
+  const $searchAll = document.querySelector('.main_search-container > input')
+  // const $lumos = document.querySelector('.lumos')
+  console.log($searchAll)
+  $searchBack.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+      if (
+        e.target.value === 'LUMOS'
+        || e.target.value === 'Lumos'
+        || e.target.value === '루모스'
+        || e.target.value === 'lumos') {
+          document.querySelector('body').classList.add('start-lumos');
+        console.log(e.target.value)
+        
+        function update(e) {
+          var x = e.clientX || e.touches[0].clientX
+          var y = e.clientY || e.touches[0].clientY
+      
+          document.querySelector('body').style.setProperty('--cursorX', x + 'px')
+          document.querySelector('body').style.setProperty('--cursorY', y + 'px')
+        }
+      
+        document.querySelector('body').addEventListener('mousemove', update)
+        document.querySelector('body').addEventListener('touchmove', update)
+      }
+    }
+  })
+
+
+
   // main_wide-menu 메뉴 펼치기
   const $meneWideBtn = document.querySelector("#main_wide-menu");
   const $WideTap = document.querySelector("#main_menu-toggle-wrap");
