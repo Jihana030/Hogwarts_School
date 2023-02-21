95819ab0aea14b0073da3c6bfdef7c537d93bc9a
Jihana030 <wldnjs7618@naver.com>
2023-02-21 10:41:30 +0900

    link test 4

--- static/js/alohomora_start.js
+++ static/js/alohomora_start.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict'
 
     // ------------start--------------------start--------------------------------------
@@ -6,21 +6,21 @@
     const start_right = document.querySelector('.start_backChange_right');
     const start_left = document.querySelector('.start_backChange_left');
     const $audio = document.querySelector('audio');
-    
-    alohomora.addEventListener('keyup', e=> {
-        if(e.keyCode===13){
-            if(e.target.value==='Alohomora' 
-                || e.target.value==='alohomora' 
-                || e.target.value==='알로호모라' 
-                || e.target.value==='ALOHOMORA'
-                ){
+
+    alohomora.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+            if (e.target.value === 'Alohomora'
+                || e.target.value === 'alohomora'
+                || e.target.value === '알로호모라'
+                || e.target.value === 'ALOHOMORA'
+            ) {
                 start_left.classList.add('start_ani_left');
                 start_right.classList.add('start_ani_right');
                 alohomora.classList.add('displayNone');
                 var meta = document.createElement('meta');
                 meta.httpEquiv = 'refresh'
                 meta.name = 'viewport';
-                meta.content = "1.6;URL='/hogwarts/main'";
+                meta.content = "1.6;URL='/HarryPotter_project/views/index.html'";
                 document.getElementsByTagName('head')[0].appendChild(meta);
                 $audio.play();
             } else {
